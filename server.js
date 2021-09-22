60a00d796bb2631bbe8b626b94942764cbe02dba
pqbao <baob1906431@student.ctu.edu.vn>
2021-09-22 14:11:41 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const config = require("./app/config");
-
+const setupContactRoutes = require("./app/routes/contact.routes");
 const app = express();
 
 app.use(cors({ origin: config.app.origins }));
@@ -13,6 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
+setupContactRoutes(app);
 
 const PORT = config.app.port;
 app.listen(PORT, () => {
